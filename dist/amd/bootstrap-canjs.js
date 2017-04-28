d7dc6fed08f9b5898d8a0cc3d4eb556b13c6d684
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-04-28 02:11:02 -0400

    0.1.0

---
+++ dist/amd/bootstrap-canjs.js
@@ -0,0 +1,8 @@
+/*bootstrap-canjs@0.0.1#bootstrap-canjs*/
+define(['exports'], function (exports) {
+    'use strict';
+    Object.defineProperty(exports, '__esModule', { value: true });
+    exports.default = function () {
+        return 'This is the bootstrap-canjs plugin';
+    };
+});
---
+++ dist/cjs/bootstrap-canjs.js
@@ -0,0 +1,6 @@
+/*bootstrap-canjs@0.0.1#bootstrap-canjs*/
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.default = function () {
+    return 'This is the bootstrap-canjs plugin';
+};
